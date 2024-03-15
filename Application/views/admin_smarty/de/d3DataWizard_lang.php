42fe5c8ca25ad8d4243956c5bcc7dad4d68d30dc
Daniel Seifert <ds@shopmodule.com>
2024-03-15 11:55:57 +0100

    adjust translation file structure

---
+++ Application/views/admin_smarty/de/d3DataWizard_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * https://www.d3data.de
+ *
+ * @copyright (C) D3 Data Development (Inh. Thomas Dartsch)
+ * @author    D3 Data Development - Daniel Seifert <info@shopmodule.com>
+ * @link      https://www.oxidmodule.com
+ */
+
+// @codeCoverageIgnoreStart
+declare(strict_types=1);
+
+$sLangName = 'Deutsch';
+
+// -------------------------------
+// RESOURCE IDENTITFIER = STRING
+// -------------------------------
+
+$aLang = include __DIR__."/../../de/d3DataWizard_translations.php";
+
+// @codeCoverageIgnoreEnd
